01c2b1c85bce9ae54bc0d7c74c551bdcd4b81e7a
mityaycho <mityaycho@gmail.com>
2018-12-07 17:46:58 +0300

    hoc

--- hoc/highlighted-blocks/js/List.js
+++ hoc/highlighted-blocks/js/List.js
@@ -1,17 +1,41 @@
 'use strict';
 
 const List = props => {
-    return props.list.map(item => {
-        switch (item.type) {
-            case 'video':
-                return (
-                    <Video {...item} />
-                );
-
-            case 'article':
-                return (
-                    <Article {...item} />
-                );
-        }
-    });
+	return (
+		props.list.map(item => {
+		switch (item.type) {
+			case 'video': 
+				return <VideoWithWrap {...item} view={item.view} />;
+			case 'article':
+				return <ArticleWidthWrap {...item} view={item.view} />;
+		}
+	})
+	);
 };
+
+const VideoWithWrap = widthStatus(Video),
+ArticleWidthWrap = widthStatus(Article);
+
+function widthStatus(Component) {
+	return (
+		class extends React.Component {
+			render() {
+				if (this.props.views > 999) {
+					return (
+						<Popular>
+							<Component {...this.props} />
+						</Popular>
+					);
+				} else if (this.props.views < 101) {
+					return (
+						<New>
+							<Component {...this.props} />
+						</New>
+					);
+				} else {
+					return <Component {...this.props} />
+				}
+			}
+		}
+	)
+}
